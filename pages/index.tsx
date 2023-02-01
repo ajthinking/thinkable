0dbd8c4b2d314a48c0c0ec7282604452edaf70e5
Anders Jurisoo <jurisoo@hotmail.com>
2023-02-01 17:07:05 +0100

    Style changes

--- pages/index.tsx
+++ pages/index.tsx
@@ -17,29 +17,30 @@ export default function Home() {
         <div className="text-yellow-400">THINKABLE</div>
         <div className="ml-2 sm:ml-4 text-gray-100">API</div>
       </div>
-      <div className="justify-left px-2 sm:justify-center flex text-gray-200 px-2">Unthinkable APIs made tinkerable using chat GPT</div>
+      <div className="justify-left px-2 sm:justify-center flex text-gray-200 px-2">Any API you can think of powered by chat GPT</div>
       <div className="px-2 mt-8 w-full max-w-2xl flex items-center justify-center space-x-2 self-center font-mono text-gray-700">
-        <input
+        {!isRedirecting && (<input
           value={apiEndPoint}
           onChange={(e) => setApiEndpoint(e.target.value)}
           placeholder="/api/magic-frogs/abilities"
-          className="flex w-full max-w-2xl items-center justify-center self-center rounded bg-gray-300 py-1 px-2 font-mono text-gray-700" />
+          className="flex w-full max-w-2xl items-center justify-center self-center rounded bg-gray-300 py-1 px-2 font-mono text-gray-700"
+        />)}
 
-          {!isRedirecting && (
-            <div
-              onClick={goToApiEndpoint}
-              className="cursor-pointer flex items-center justify-center self-center rounded bg-yellow-400 py-1 px-2 font-mono text-gray-700"
-            >Go!</div>
-          )}
-          {isRedirecting && (
-            <div className="flex items-center justify-center self-center rounded bg-yellow-400 py-1 px-2 font-mono text-gray-700 animate-pulse">Building your API...</div>
-          )}
-        </div>
-      <div className="w-full px-2 sm:px-12">      
+        {!isRedirecting && (
+          <div
+            onClick={goToApiEndpoint}
+            className="cursor-pointer flex items-center justify-center self-center rounded bg-yellow-400 py-1 px-2 font-mono text-gray-700"
+          >Go!</div>
+        )}
+        {isRedirecting && (
+          <div className="flex items-center justify-center self-center rounded bg-yellow-400 py-1 px-2 font-mono text-gray-700 animate-pulse">Building your API...</div>
+        )}
+      </div>
+      <div className="flex flex-col justify-center w-full px-2 sm:px-12">      
           {isRedirecting && (
             <div className="flex items-center justify-center self-center text-xs mt-4 mb-2 font-mono text-gray-700">First time requests are painfully slow. Then we will cache it.</div>
           )}      
-        <img alt="result" className="mt-12 shadow-2xl" src="https://i.imgur.com/GN0pSdU.png" />    
+        <img alt="result" className="self-center mt-12 shadow-2xl sm:max-w-2xl" src="https://i.imgur.com/GN0pSdU.png" />    
       </div>
     </div>
   )
