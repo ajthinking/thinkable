33f3357362752a2655f67250e7d2d5004c684440
Anders Jurisoo <jurisoo@hotmail.com>
2023-02-01 16:49:01 +0100

    Add responsive styles

--- pages/index.tsx
+++ pages/index.tsx
@@ -12,12 +12,12 @@ export default function Home() {
   }
 
   return (
-    <div className="bg-gradient-to- flex flex-col bg-sky-500 pt-24 pb-24 shadow-xl ring-1 ring-gray-900/5">
-      <div className="mx-auto flex text-6xl font-black">
+    <div className="bg-gradient-to- flex flex-col pt-4 sm:pt-12 pb-24 ring-1 ring-gray-900/5">
+      <div className="justify-left px-2 sm:justify-center flex text-2xl sm:text-6xl font-black">
         <div className="text-yellow-400">THINKABLE</div>
-        <div className="ml-4 text-gray-100">API</div>
+        <div className="ml-2 sm:ml-4 text-gray-100">API</div>
       </div>
-      <div className="mx-auto text-gray-200">Unthinkable APIs made tinkerable using chat GPT</div>
+      <div className="mx-auto text-gray-200 px-2">Unthinkable APIs made tinkerable using chat GPT</div>
       <div className="mt-8 flex items-center justify-center space-x-2 self-center font-mono text-gray-700">
         <input
           value={apiEndPoint}
@@ -35,7 +35,7 @@ export default function Home() {
             <div className="flex items-center justify-center self-center rounded bg-yellow-400 py-1 px-2 font-mono text-gray-700 animate-pulse">Building your API...</div>
           )}
         </div>
-      <div className="w-full px-12">      
+      <div className="w-full sm:px-12">      
           {isRedirecting && (
             <div className="flex items-center justify-center self-center text-xs mt-4 mb-2 font-mono text-gray-700">First time requests are painfully slow. Then we will cache it.</div>
           )}      
