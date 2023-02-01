0ddc3b08b153a0ae1d82354065f02aa3b2c27a2b
Anders Jurisoo <jurisoo@hotmail.com>
2023-02-01 10:17:17 +0100

    Dont show token

--- pages/api/[...endpoint].ts
+++ pages/api/[...endpoint].ts
@@ -8,8 +8,8 @@ export const revalidate = 3600 * 24 * 7 * 52;
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
   try {
     res.status(200).json(await getImaginedResponse(req));
-  } catch (error) {
-    res.status(500).json({ error, message: "Im very sorry there was an error" });
+  } catch (error: any) {
+    res.status(500).json({ message: error.message });
   }
 }
 
