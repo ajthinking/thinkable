4a433efab379495e45557b0f930ca4f722092cb5
Anders Jurisoo <jurisoo@hotmail.com>
2023-02-01 16:59:18 +0100

    Responsive changes

--- pages/index.tsx
+++ pages/index.tsx
@@ -17,13 +17,13 @@ export default function Home() {
         <div className="text-yellow-400">THINKABLE</div>
         <div className="ml-2 sm:ml-4 text-gray-100">API</div>
       </div>
-      <div className="mx-auto text-gray-200 px-2">Unthinkable APIs made tinkerable using chat GPT</div>
-      <div className="mt-8 flex items-center justify-center space-x-2 self-center font-mono text-gray-700">
+      <div className="justify-left px-2 sm:justify-center flex text-gray-200 px-2">Unthinkable APIs made tinkerable using chat GPT</div>
+      <div className="px-2 mt-8 w-full max-w-2xl flex items-center justify-center space-x-2 self-center font-mono text-gray-700">
         <input
           value={apiEndPoint}
           onChange={(e) => setApiEndpoint(e.target.value)}
           placeholder="/api/magic-frogs/abilities"
-          className="flex items-center justify-center self-center rounded bg-gray-300 py-1 px-2 font-mono text-gray-700" />
+          className="flex w-full max-w-2xl items-center justify-center self-center rounded bg-gray-300 py-1 px-2 font-mono text-gray-700" />
 
           {!isRedirecting && (
             <div
@@ -35,7 +35,7 @@ export default function Home() {
             <div className="flex items-center justify-center self-center rounded bg-yellow-400 py-1 px-2 font-mono text-gray-700 animate-pulse">Building your API...</div>
           )}
         </div>
-      <div className="w-full sm:px-12">      
+      <div className="w-full px-2 sm:px-12">      
           {isRedirecting && (
             <div className="flex items-center justify-center self-center text-xs mt-4 mb-2 font-mono text-gray-700">First time requests are painfully slow. Then we will cache it.</div>
           )}      
