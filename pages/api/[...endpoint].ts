3c85f9f30164aeaf46c0ffcc5ed0e2e26b8563d1
Anders Jurisoo <jurisoo@hotmail.com>
2023-02-01 10:12:09 +0100

    Initial scaffolding

---
+++ pages/api/[...endpoint].ts
@@ -0,0 +1,60 @@
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import type { NextApiRequest, NextApiResponse } from "next";
+import { Configuration, OpenAIApi } from "openai";
+
+// TODO HOW CACHE THE RESPONSES?
+export const revalidate = 3600 * 24 * 7 * 52;
+
+export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+  try {
+    res.status(200).json(await getImaginedResponse(req));
+  } catch (error) {
+    res.status(500).json({ error, message: "Im very sorry there was an error" });
+  }
+}
+
+export const getImaginedResponse = async (req: NextApiRequest) => {
+  const openai = new OpenAIApi(
+    new Configuration({
+      apiKey: process.env.OPEN_AI_SECRET,
+    })
+  );
+
+  // TODO SEND A REQUEST TO SPAM/ABUSE PROTECTION ENDPOINT FIRST
+
+  const completion = await openai.createCompletion({
+    model: "text-davinci-003",
+    prompt: `Imagine the JSON response of a GET call to a endpoint ${rebuildEndpoint(req.query)}. Give nothing but the imagined JSON please, that is, provide no explanation before or after the code.`,
+    temperature: 0,
+    max_tokens: 500,
+    top_p: 1,
+    frequency_penalty: 0.0,
+    presence_penalty: 0.0,
+  });
+
+  const responseContent = completion.data.choices[0].text as string;
+
+  const responseJson = JSON.parse(responseContent);
+  if (!responseJson) throw new Error("Could not parse JSON");
+
+  return responseJson;
+};
+
+export const rebuildEndpoint = (query: any): string => {
+  let endpoint = query.endpoint.join("/");
+
+  const keys = Object.keys(query).filter((key) => key !== "endpoint");
+
+  if (keys.length > 0) {
+    endpoint += "?";
+  }
+
+  keys.forEach((key, index) => {
+    endpoint += `${key}=${query[key]}`;
+    if (index < keys.length - 1) {
+      endpoint += "&";
+    }
+  });
+
+  return encodeURI(endpoint);
+};
